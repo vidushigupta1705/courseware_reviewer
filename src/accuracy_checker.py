f0b7768cbd23eef5d7d75b0172b12c9a0582c073
vidushigupta1705 <vidushi017@gmail.com>
2026-05-08 17:11:53 +0530

    Update accuracy_checker.py (uniform sdk)

--- src/accuracy_checker.py
+++ src/accuracy_checker.py
@@ -3,7 +3,7 @@ import os
 from typing import List, Dict
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from mistralai.client import Mistral
+from mistralai import Mistral
 
 from config import (
     ENABLE_ACCURACY_CHECK,
