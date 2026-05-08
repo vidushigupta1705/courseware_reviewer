861442d69cecbcdcb95f0c0206ed29059a9dfb30
vidushigupta1705 <vidushi017@gmail.com>
2026-05-08 17:11:28 +0530

    Update llm_rewrite.py (uniform sdk)

--- src/llm_rewrite.py
+++ src/llm_rewrite.py
@@ -3,7 +3,7 @@ import os
 from typing import List, Dict, Set
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from mistralai.client import Mistral
+from mistralai import Mistral
 
 from config import (
     ENABLE_LLM_REWRITE,
