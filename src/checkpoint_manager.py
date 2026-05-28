4b9f793fea1d19872e6817404669d9090592ea82
vidushigupta1705 <vidushi017@gmail.com>
2026-05-28 17:24:56 +0530

    Update checkpoint_manager.py

--- src/checkpoint_manager.py
+++ src/checkpoint_manager.py
@@ -89,6 +89,22 @@ def _load_units(items):
 def _load_issues(items):
     return [ReviewIssue(**x) for x in items]
 
+def _load_visual_specs(items):
+    from models import VisualSpec, VisualNode, VisualEdge
+    specs = []
+    for item in items:
+        nodes = [VisualNode(**n) for n in item.get("nodes", [])]
+        edges = [VisualEdge(**e) for e in item.get("edges", [])]
+        item_copy = dict(item)
+        item_copy["nodes"] = nodes
+        item_copy["edges"] = edges
+        specs.append(VisualSpec(**item_copy))
+    return specs
+
+def _load_generated_visuals(items):
+    from models import GeneratedVisual
+    return [GeneratedVisual(**x) for x in items]
+
 
 def load_checkpoint(base_filename: str, stage_name: str):
     path = CHECKPOINT_DIR / f"{base_filename}__{stage_name}.json"
@@ -107,8 +123,8 @@ def load_checkpoint(base_filename: str, stage_name: str):
         rewrite_suggestions=_load_rewrite_suggestions(data.get("rewrite_suggestions", [])),
         units=_load_units(data.get("units", [])),
         issues=_load_issues(data.get("issues", [])),
-        visual_specs=data.get("visual_specs", []),      
-        generated_visuals=data.get("generated_visuals", []),  
+        visual_specs=_load_visual_specs(data.get("visual_specs", [])),
+        generated_visuals=_load_generated_visuals(data.get("generated_visuals", [])),
     )
 
     for attr in _DYNAMIC_ATTRS:
